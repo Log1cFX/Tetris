0fbf523edb159501285f4f9d4e640e85b6a521cc
Log1cFX <raffisand@gmail.com>
2024-02-27 19:36:15 +0100

    Comments added + more

--- Logic/Inputs/Inputs.java
+++ Logic/Inputs/Inputs.java
@@ -13,21 +13,26 @@ public class Inputs implements KeyListener {
 	public void keyTyped(KeyEvent e) {
 	}
 
+	// creates new instance of KeyPressed
 	@Override
 	public void keyPressed(KeyEvent e) {
 		boolean addKey = true;
-		for (KeyPressed key : KeysArray) { // Hello world
+		for (KeyPressed key : KeysArray) {
 			if (key.KeyCode == e.getKeyCode()) {
 				addKey = false;
 				break;
 			}
 		}
-		if (addKey) KeysArray.add(new KeyPressed(e.getKeyCode()));
+		// checks all the existing instances of keyPressed to see if there is an
+		// instance with the same button
+		if (addKey)
+			KeysArray.add(new KeyPressed(e.getKeyCode()));
+		// adds instance of keyPressed with the key code of the current button
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		for(int i=0;i<KeysArray.size();i++) {
+		for (int i = 0; i < KeysArray.size(); i++) {
 			if (KeysArray.get(i).KeyCode == e.getKeyCode()) {
 				KeysArray.get(i).shutdown();
 				KeysArray.remove(i);
