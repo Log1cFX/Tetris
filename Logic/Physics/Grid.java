94a999497ff923aa51f3caa976e29dd0d3076e9e
Naloxu <naroeri42@gmail.com>
2024-02-27 21:15:02 +0100

    Update Grid.java

--- Logic/Physics/Grid.java
+++ Logic/Physics/Grid.java
@@ -3,6 +3,7 @@ package Physics;
 import java.awt.Point;
 
 import ScreenSettings.Settings;
+import Sprites.ColorPalette;
 
 
 // all the info and methods for the grid
@@ -37,7 +38,24 @@ public class Grid extends Block implements Update{
 			Placed_Blocks[pos.x][pos.y] = CurrentBlock.getColorPalette();
 		}
 	}
-	
+	private static void BurnLines() {
+		boolean[] isFullLine = new boolean[Settings.COLUMNS];
+		for(int columns = 0;columns <= Settings.COLUMNS;columns++) {
+			for(int rows = 0;rows <= Settings.ROWS;rows++) {
+				if (Placed_Blocks[rows][columns] == null) {
+					isFullLine[columns] = false;
+					break;
+				}
+			}
+			if (!isFullLine[columns]) {
+				isFullLine[columns] = true;
+			}
+		}
+		ColorPalette[][] newGrid = new ColorPalette[Settings.ROWS][Settings.COLUMNS];
+		for (int columns = Settings.COLUMNS;columns >= 0;columns--) {
+			
+		}
+	}
 
 	@Override
 	public void start() {
