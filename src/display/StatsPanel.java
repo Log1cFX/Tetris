f4b4cdb868d57d69f6368cda8d7d51a02b80c183
Naloxu <naroeri42@gmail.com>
2024-03-14 22:31:22 +0100

    HELP
    I can't do this thing with your idea that I don't know that's existed

--- src/display/StatsPanel.java
+++ src/display/StatsPanel.java
@@ -3,8 +3,6 @@ package display;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
@@ -25,30 +23,18 @@ public class StatsPanel extends JPanel implements GraphicsUpdate{
 	
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.gray);
-		g.drawLine(0,80,400,80); // Separate between the game panel and the statistics panel
-		
 		displayScore(g);
 		
-		g.setColor(Color.gray);
-		g.drawLine(0,0,0,800); // create a case for the score
+
 		// System.out.println("PaintComponent: "+Thread.currentThread());
 	}
 	
-	private void displayScore(Graphics g) {
-		Font font = new Font("Arial Black", Font.BOLD, 40);
-		g.setFont(font);
-		g.setColor(Color.white);
-		g.drawString(String.valueOf(CurrentGameState.score), 85, 57);
+	private void displayScore(Graphics g) { // Displaying the score
+
 	}
 	
-	private void displayNextBlock(Graphics g) {
-		Font font = new Font("Arial Black", Font.BOLD, 40);
-		g.setFont(font);
-		g.setColor(Color.white);
-		
-		FontMetrics metrics = g.getFontMetrics(font);
-		
+	private void displayNextBlock(Graphics g) { // Displaying the next block
+
 	}
 
 
