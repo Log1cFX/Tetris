09fe2583d72bdede6b1c1993914755e9f24e774b
Log1cFX <raffisand@gmail.com>
2024-03-14 23:28:04 +0100

    Squashed commit of the following:
    commit cb7e59a9959203bc3547cc2da26894dd82451c97
    Author: Log1cFX <raffisand@gmail.com>
    Date:   Thu Mar 14 23:09:45 2024 +0100
    
        brother i helped you a litle bit
    
    commit f4b4cdb868d57d69f6368cda8d7d51a02b80c183
    Author: Naloxu <naroeri42@gmail.com>
    Date:   Thu Mar 14 22:31:22 2024 +0100
    
        HELP
    
        I can't do this thing with your idea that I don't know that's existed
    
    commit f730f3ce1449739cded6c731ddbed276a5e06529
    Author: Naloxu <naroeri42@gmail.com>
    Date:   Thu Mar 14 22:02:17 2024 +0100
    
        rectified problems
    
    commit d74f3a1507da99702b9eed772747f3c514693335
    Author: Naloxu <naroeri42@gmail.com>
    Date:   Thu Mar 14 21:35:35 2024 +0100
    
        StatsPanel
    
        the Stats Panel is added

--- src/display/Frame.java
+++ src/display/Frame.java
@@ -1,11 +1,16 @@
 package display;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 
 public class Frame extends JFrame {
-    Panel panel = new Panel();
 
     Frame() {
-    	this.add(new Panel());
+    	this.setLayout(new BorderLayout());
+    	this.add(new Panel(), BorderLayout.WEST);
+    	this.add(new StatsPanel(), BorderLayout.EAST);
 		this.setTitle("Tertrix");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
---
+++ src/display/StatsPanel.java
@@ -0,0 +1,61 @@
+package display;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import javax.swing.JPanel;
+
+import logic.physics.GraphicsUpdate;
+import logic.physics.PhysicsLoopCaster;
+import settings.CurrentGameState;
+import settings.Settings;
+
+public class StatsPanel extends JPanel implements GraphicsUpdate {
+
+	Rectangle scoreRect = new Rectangle(0, (Settings.Screen.SCREEN_HEIGHT / 3) * 0, Settings.Screen.SCREEN_WIDTH / 2,
+			Settings.Screen.SCREEN_HEIGHT / 3);
+	Rectangle nextBlockRect = new Rectangle(0, (Settings.Screen.SCREEN_HEIGHT / 3) * 1,
+			Settings.Screen.SCREEN_WIDTH / 2, Settings.Screen.SCREEN_HEIGHT / 3);
+	Rectangle levelRect = new Rectangle(0, (Settings.Screen.SCREEN_HEIGHT / 3) * 2,
+			Settings.Screen.SCREEN_WIDTH / 2, Settings.Screen.SCREEN_HEIGHT / 3);
+
+	StatsPanel() {
+		this.setBackground(Color.BLACK);
+		this.setPreferredSize(new Dimension(Settings.Screen.SCREEN_WIDTH / 2, Settings.Screen.SCREEN_HEIGHT));
+		this.setFocusable(true);
+		PhysicsLoopCaster.addGraphicsUpdateLoop(this);
+	}
+
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g;
+		drawStatFrames(g2d);
+
+		// System.out.println("PaintComponent: "+Thread.currentThread());
+	}
+
+	// Drawing Frames (decorative)
+	private void drawStatFrames(Graphics2D g2d) { 
+		g2d.setStroke(new BasicStroke(3));
+		g2d.setColor(Color.white);
+		g2d.draw(scoreRect);
+		g2d.draw(levelRect);
+		g2d.draw(nextBlockRect);
+	}
+
+	private void displayNextBlock(Graphics2D g2d) { // Displaying the next block
+
+	}
+
+	@Override
+	public void Update() {
+		repaint();
+
+	}
+
+}
