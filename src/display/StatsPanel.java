cb7e59a9959203bc3547cc2da26894dd82451c97
Log1cFX <raffisand@gmail.com>
2024-03-14 23:09:45 +0100

    brother i helped you a litle bit

--- src/display/StatsPanel.java
+++ src/display/StatsPanel.java
@@ -1,8 +1,12 @@
 package display;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 import javax.swing.JPanel;
 
@@ -11,37 +15,47 @@ import logic.physics.PhysicsLoopCaster;
 import settings.CurrentGameState;
 import settings.Settings;
 
-public class StatsPanel extends JPanel implements GraphicsUpdate{
-	
+public class StatsPanel extends JPanel implements GraphicsUpdate {
+
+	Rectangle scoreRect = new Rectangle(0, (Settings.Screen.SCREEN_HEIGHT / 3) * 0, Settings.Screen.SCREEN_WIDTH / 2,
+			Settings.Screen.SCREEN_HEIGHT / 3);
+	Rectangle nextBlockRect = new Rectangle(0, (Settings.Screen.SCREEN_HEIGHT / 3) * 1,
+			Settings.Screen.SCREEN_WIDTH / 2, Settings.Screen.SCREEN_HEIGHT / 3);
+	Rectangle levelRect = new Rectangle(0, (Settings.Screen.SCREEN_HEIGHT / 3) * 2,
+			Settings.Screen.SCREEN_WIDTH / 2, Settings.Screen.SCREEN_HEIGHT / 3);
+
 	StatsPanel() {
 		this.setBackground(Color.BLACK);
 		this.setPreferredSize(new Dimension(Settings.Screen.SCREEN_WIDTH / 2, Settings.Screen.SCREEN_HEIGHT));
 		this.setFocusable(true);
 		PhysicsLoopCaster.addGraphicsUpdateLoop(this);
-		
 	}
-	
+
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		displayScore(g);
-		
+		Graphics2D g2d = (Graphics2D) g;
+		drawStatFrames(g2d);
 
 		// System.out.println("PaintComponent: "+Thread.currentThread());
 	}
-	
-	private void displayScore(Graphics g) { // Displaying the score
+
+	// Drawing Frames (decorative)
+	private void drawStatFrames(Graphics2D g2d) { 
+		g2d.setStroke(new BasicStroke(3));
+		g2d.setColor(Color.white);
+		g2d.draw(scoreRect);
+		g2d.draw(levelRect);
+		g2d.draw(nextBlockRect);
+	}
+
+	private void displayNextBlock(Graphics2D g2d) { // Displaying the next block
 
 	}
-	
-	private void displayNextBlock(Graphics g) { // Displaying the next block
-
-	}
-
 
 	@Override
 	public void Update() {
 		repaint();
-		
+
 	}
 
 }
